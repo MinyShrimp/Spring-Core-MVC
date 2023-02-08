00b0c5736bbcc6a510e63249d749a46d6aeeae76
MinyShrimp <ksk7584@gmail.com>
2023-02-08 19:56:53 +0900

    프로젝트 세팅

---
+++ src/main/java/hello/springcoremvc/SpringCoreMvcApplication.java
@@ -0,0 +1,13 @@
+package hello.springcoremvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springcoremvc/SpringCoreMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcoremvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
